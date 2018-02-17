9e38919ee8a35ad571ae0efa2ac4c7fcae4ba8ee
Unknown <Rosenumber14@yahoo.com>
2018-02-16 22:05:29 -0700

    add redux-thunk

--- Fontend/src/index.js
+++ Fontend/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import Thunk from 'redux-thunk';
 
 import App from './components/App';
 import UI from './Reducers/UI';
 
 const store = createStore(combineReducers({
   UI
-}));
+}), applyMiddleware(Thunk));
 
 ReactDOM.render(
   <Provider store={store}>
